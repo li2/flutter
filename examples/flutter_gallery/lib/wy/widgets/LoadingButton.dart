9462f2cd085f41c53c2448c9b150e9dd27d9971c
Weiyi Li <weiyi.just2@gmail.com>
2019-02-03 16:28:33 +1100

    Add a button widget which support 3 states: idle, loading , disabled (#1)

---
+++ examples/flutter_gallery/lib/wy/widgets/LoadingButton.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+enum LoadingButtonState {
+  IDLE, LOADING, DISABLED
+}
+
+class LoadingButton extends StatelessWidget {
+  const LoadingButton({
+    @required this.state,
+    this.text,
+    this.textColor,
+    this.backgroundColor,
+    this.progressColor,
+    this.onPressed,
+  });
+
+  final LoadingButtonState state;
+  final String text;
+  final Color textColor;
+  final Color backgroundColor;
+  final Color progressColor;
+  final GestureTapCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Opacity(
+          opacity: state != LoadingButtonState.LOADING ? 1.0 : 0.0,
+          child: RaisedButton(
+            child: Text(state != LoadingButtonState.LOADING ? text : '', style: TextStyle(color: textColor)),
+            onPressed: state == LoadingButtonState.IDLE ? onPressed : null,
+            color: backgroundColor,
+          ),
+        ),
+        Opacity(
+          opacity: state == LoadingButtonState.LOADING ? 1.0 : 0.0,
+          child: CircularProgressIndicator(valueColor: AlwaysStoppedAnimation<Color>(progressColor)),
+        )
+      ]);
+  }
+}
